be6c6fe8aa930c48f5e4d398ea9e7f9f7cbcfe78
lunatixmyscript <scriptlunatixtunnel293@gmail.com>
2024-06-27 01:27:15 +0700

    Update Example_Service.sh

--- Example_Service.sh
+++ Example_Service.sh
@@ -15,3 +15,15 @@ WantedBy=multi-user.target
 # */ */ */ */ */ */ */ */ */
 # */ Service Limit Quota \*
 
+[Unit]
+Description=Check and Manage Vmess Quota Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash -c '/usr/local/sbin/quotavme'
+Restart=always
+RestartSec=1 # akan di jalankan setiap 5 menit
+
+[Install]
+WantedBy=multi-user.target
