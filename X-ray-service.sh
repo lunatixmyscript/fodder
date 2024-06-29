5cc8dac1da72a2211c9edbed4d9fdffb82361d8b
lunatixmyscript <scriptlunatixtunnel293@gmail.com>
2024-06-29 20:39:27 +0700

    Update X-ray-service.sh

--- X-ray-service.sh
+++ X-ray-service.sh
@@ -65,11 +65,72 @@ StartLimitInterval=0
 WantedBy=multi-user.target
 EOF
 
+
+# // service Kuota Xray
+cat >/etc/systemd/system/ssr-quota.service << EOF
+[Unit]
+Description=Auto Kill Accounts Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/sbin/quotassr
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat >/etc/systemd/system/tro-quota.service << EOF
+[Unit]
+Description=Auto Kill Accounts Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/sbin/quotatro
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat >/etc/systemd/system/vme-quota.service << EOF
+[Unit]
+Description=Auto Kill Accounts Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/sbin/quotavle
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat >/etc/systemd/system/vle-quota.service << EOF
+[Unit]
+Description=Auto Kill Accounts Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/sbin/quotavme
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 chmod +x /etc/systemd/system/autolock-sdwsk.service
 chmod +x /etc/systemd/system/autolock-vmess.service
 chmod +x /etc/systemd/system/autolock-vless.service
 chmod +x /etc/systemd/system/autolock-trjan.service
-
+chmod +x /etc/systemd/system/quota-vle.service
+chmod +x /etc/systemd/system/quota-vme.service
+chmod +x /etc/systemd/system/quota-tro.service
+chmod +x /etc/systemd/system/quota-ssr.service
 
 clear
 echo -e "\e[92;1m MEMUAT ULANG CONFIG SERVICE \e[0m"
@@ -87,6 +148,19 @@ sleep 1
 echo -e "\e[92;1m RESTART TROJAN \e[0m"
 systemctl restart autolock-trjan
 sleep 1
+echo -e "\e[92;1m RESTART TROJAN QUOTA\e[0m"
+systemctl restart quota-tro
+sleep 1
+echo -e "\e[92;1m RESTART SDWSK QUOTA \e[0m"
+systemctl restart quota-ssr
+sleep 1
+echo -e "\e[92;1m RESTART VMESS QUOTA \e[0m"
+systemctl restart quota-vme
+sleep 1
+echo -e "\e[92;1m RESTART VLESS QUOTA \e[0m"
+systemctl restart quota-vle
+sleep 1
+
 echo -e "\e[92;1m MENJALANKAN SERVICE VMESS \e[0m"
 systemctl enable autolock-vmess
 sleep 1
@@ -98,5 +172,17 @@ systemctl enable autolock-sdwsk
 sleep 1
 echo -e "\e[92;1m MENJALANKAN SERVICE TROJAN \e[0m"
 systemctl enable autolock-trjan
+sleep 1
+echo -e "\e[92;1m MENJALANKAN SERVICE TROJAN QUOTA \e[0m"
+systemctl enable quota-tro
+sleep 1
+echo -e "\e[92;1m MENJALANKAN SERVICE SDWSK QUOTA \e[0m"
+systemctl enable quota-ssr
+sleep 1
+echo -e "\e[92;1m MENJALANKAN SERVICE VLESS QUOTA \e[0m"
+systemctl enable quota-vle
+sleep 1
+echo -e "\e[92;1m MENJALANKAN SERVICE VMESS QUOTA \e[0m"
+systemctl enable quota-vme
 sleep 1
 clear
